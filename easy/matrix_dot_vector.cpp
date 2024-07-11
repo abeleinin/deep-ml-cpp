a923fc952f57557667bb63fe11a53d8363a09480
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-07-10 23:43:44 -0400

    matrix times vector nits

--- easy/matrix_dot_vector.cpp
+++ easy/matrix_dot_vector.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 vector<float> matrix_dot_vector(const vector<vector<float> >& m, const vector<float>& v)
 {
+    // N x M matrix needs vector of length M
     assert(m[0].size() == v.size());
 
     vector<float> res(v.size(), 0.0);
-
     for (int i = 0; i < m.size(); i++){
         for (int j = 0; j < m[0].size(); j++){
             res[i] += v[j] * m[i][j];
@@ -20,12 +20,17 @@ vector<float> matrix_dot_vector(const vector<vector<float> >& m, const vector<fl
 
 int main(void)
 {
-    vector<vector<float> > a = {{1, 2}, {2, 4}};
-    vector<float> b = {1, 2};
-    vector<float> exp = {5, 10};
-    vector<float> res = matrix_dot_vector(a, b);
+    vector<vector<float> > a;
+    vector<float> b, exp, res;
+
+    // Test 1
+    a = {{1, 2}, {2, 4}};
+    b = {1, 2};
+    exp = {5, 10};
+    res = matrix_dot_vector(a, b);
     assert(res == exp);
 
+    // Test 2
     a = {{1, 2, 3}, {3, 4, 5}, {6, 7, 8}};
     b = {1, 2, 3};
     exp = {14, 26, 44};
