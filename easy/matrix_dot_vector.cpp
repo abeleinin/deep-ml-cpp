86e8f5951b8ac35c56a250a2ddaf826c7f384cb6
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-07-11 22:34:17 -0400

    matrix dot vector

--- easy/matrix_dot_vector.cpp
+++ easy/matrix_dot_vector.cpp
@@ -21,21 +21,21 @@ vector<float> matrix_dot_vector(const vector<vector<float> >& m, const vector<fl
 int main(void)
 {
     vector<vector<float> > a;
-    vector<float> b, exp, res;
+    vector<float> b, expect, result;
 
     // Test 1
     a = {{1, 2}, {2, 4}};
     b = {1, 2};
-    exp = {5, 10};
-    res = matrix_dot_vector(a, b);
-    assert(res == exp);
+    expect = {5, 10};
+    result = matrix_dot_vector(a, b);
+    assert(result == expect);
 
     // Test 2
     a = {{1, 2, 3}, {3, 4, 5}, {6, 7, 8}};
     b = {1, 2, 3};
-    exp = {14, 26, 44};
-    res = matrix_dot_vector(a, b);
-    assert(res == exp);
+    expect = {14, 26, 44};
+    result = matrix_dot_vector(a, b);
+    assert(result == expect);
 
     return 0;
 }
