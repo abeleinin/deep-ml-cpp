5d15b61711db4981707bb4044057c63651a03ada
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-07-18 01:31:19 -0400

    softmax

---
+++ easy/softmax.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+void softmax(vector<double>& z)
+{
+    double Z = 0.0;
+    for (auto& z_i : z){
+        z_i = exp(z_i);
+        Z += z_i;
+    }
+    for (auto& z_i : z){
+        z_i /= Z;
+    }
+}
+
+int main(void)
+{
+    vector<double> a = {1, 2, 3};
+    vector<double> expected = {0.0900, 0.2447, 0.6652};
+
+    softmax(a);
+    for (int i = 0; i < a.size(); i++){
+        cout << a[i] << " == " << expected[i] << endl;
+    }
+    return 0;
+}
