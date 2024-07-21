b0519410ba32dcfd2007a80f2f156460d6b5be43
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-07-20 23:37:29 -0500

    single neuron

---
+++ easy/single_neuron.cpp
@@ -0,0 +1,49 @@
+#include <Eigen/Dense>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+
+double sigmoid(double x)
+{
+    return 1 / (1 + exp(-x));
+}
+
+pair<VectorXd, float> single_neuron(const MatrixXd& features, 
+                                    const VectorXd& labels,
+                                    const VectorXd& weights, 
+                                    float bias)
+{
+    VectorXd prob(features.rows());
+    float mse = 0.0;
+    for (int i = 0; i < features.rows(); i++){
+        float z = 0.0;
+        for (int j = 0; j < features.cols(); j++){
+            z += weights(j) * features(i,j);
+        }
+        prob(i) = sigmoid(z + bias);
+        mse += pow(prob(i) - labels(i), 2);
+    }
+    mse /= features.rows();
+    return {prob, mse};
+}
+
+int main(void)
+{
+    MatrixXd features(3, 2);
+    VectorXd labels(3);
+    VectorXd weights(2);
+    features << 0.5, 1.0,
+                -1.5, -2.0,
+                2.0, 1.5;
+    labels << 0, 1, 0;
+    weights << 0.7, -0.4;
+    float bias = -0.1;
+
+    pair<VectorXd, float> result = single_neuron(features, labels, weights, bias);
+    cout << result.first << endl;
+    cout << result.second << endl;
+
+    return 0;
+}
