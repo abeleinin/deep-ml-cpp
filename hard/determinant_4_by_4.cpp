153afa00d22465267f2bd445f187abe4ee06a365
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-09-28 23:48:05 -0500

    4x4 determinant using laplace's expansion

--- hard/determinant_4_by_4.cpp
+++ hard/determinant_4_by_4.cpp
@@ -2,25 +2,33 @@
 
 using namespace std;
 
+double determinant_3_by_3(const vector<vector<double>>& m)
+{
+    double minor_00 = m[1][1] * m[2][2] - m[1][2] * m[2][1];
+    double minor_01 = m[1][0] * m[2][2] - m[1][2] * m[2][0];
+    double minor_02 = m[1][0] * m[2][1] - m[1][1] * m[2][0];
+
+    return m[0][0] * minor_00 - m[0][1] * minor_01 + m[0][2] * minor_02;
+}
+
+// https://math.emory.edu/~lchen41/teaching/2020_Fall/Section_3-6.pdf
 double determinant_4_by_4(const vector<vector<double>>& matrix)
 {
-    if (matrix.size() == 1) {
-        return matrix[0][0];
-    }
-
-    double det = 0;
-    for (int c = 0; c < matrix.size(); ++c) {
-        vector<vector<double>> minor;
-        for (int i = 1; i < matrix.size(); ++i) {
-            vector<double> row;
-            for (size_t j = 0; j < matrix[i].size(); ++j) {
-                if (j != c) row.push_back(matrix[i][j]);
+    double det = 0.0;
+    for (int c = 0; c < 4; c++) {
+        vector<vector<double>> minor(3, vector<double>(3));
+        for (int i = 1; i < 4; i++) {
+            int minor_col = 0;
+            for (int j = 0; j < 4; j++) {
+                if (j != c) {
+                    minor[i - 1][minor_col] = matrix[i][j];
+                    minor_col++;
+                }
             }
-            minor.push_back(row);
         }
-
-        double cofactor = (pow(-1, c)) * determinant_4_by_4(minor);
+        double cofactor = (c % 2 == 0 ? 1 : -1) * determinant_3_by_3(minor);
         det += matrix[0][c] * cofactor;
     }
+
     return det;
 }
